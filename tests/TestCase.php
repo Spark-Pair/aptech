da5222328f6fa7edfdca5934aae4eb9342284627
Hasan Raza <140344786+razajp@users.noreply.github.com>
2026-09-12 00:32:20 -0700

    test: isolate feature suite from persistent route throttles

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,19 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Routing\Middleware\ThrottleRequests;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        // Route throttles use cache-backed counters that can survive between local
+        // PHPUnit processes (especially with the file cache driver). They protect
+        // production traffic, but should not make unrelated functional tests flaky.
+        $this->withoutMiddleware(ThrottleRequests::class);
+    }
 }
